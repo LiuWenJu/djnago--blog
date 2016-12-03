3620a10205d8bc36576c8f9bfb1eae01c22e6b9f
LiuWenJu <lwj037303@gmail.com>
2016-12-03 10:26:26 +0800

    add dynamic url

--- article/views.py
+++ article/views.py
@@ -2,20 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
+from django.http import Http404
 
 
 # Create your views here.
 def home(request):
     post_list = Article.objects.all()
-    return render(request, 'home.html', {'post_list' : post_list})
+    return render(request, 'home.html', {'post_list':post_list})
 
 
-def detail(requestion, my_args):
+def detail(request, id):
+    """
     #return HttpResponse("You're looking at my_args %s."% my_args)
     post = Article.objects.all()[int(my_args)]
     str = ("title = %s, category = %s, date_time = %s, context = %s"
         % (post.title, post.category, post.date_time, post.content))
     return HttpResponse(str)
+    """
+    try:
+        post = Article.objects.get(id=str(id))
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'post.html', {'post':post})
 
 #def test(request):
 #    return render(request, 'test.html', {'current_time': datetime.now()})
